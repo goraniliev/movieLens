7ab2743a3ea4c89c7128fe4983423256aab51630
Your Name <goraniliev93@yahoo.com>
2015-06-10 04:24:35 +0200

    added logitRegression.R

--- R/logitRegression.R
+++ R/logitRegression.R
@@ -11,30 +11,14 @@ ind = sample(2, nrow(modelData), replace=TRUE, prob=c(0.7, 0.3))
 trainData = modelData[ind==1, ]
 testData = modelData[ind==2, ]
 
-formula = as.factor(rating) ~ age + gender +  Action + Adventure + Animation + Children +
+formula = rating ~ age + gender +  Action + Adventure + Animation + Children +
   + Comedy + Crime + Documentary + Drama + Fantasy + FilmNoir + Horror +     
   + Musical + Mystery + Romance + SciFi + Thriller + War + Western
 
-logit = glm(formula, data = trainData, family = "binomial")
-summary(logit)
 
-predicted = predict(logit, newdata = testData)
+library(nnet)
+ml = multinom(formula, data = trainData)
+predicted = predict(ml, newdata = testData)
+predicted
 trueResults = testData$rating
-summarization = confusionMatrix(as.factor(round(predicted)), as.factor(trueResults))
-
-
-
-
-
-
-
-# new
-
-install.packages('mlogit')
-library(mlogit)
-
-?mlogit
-multiLogit = mlogit(formula, data = trainData)
-formula
-head(trainData)
-
+summarization = confusionMatrix(as.factor(predicted), as.factor(trueResults))
