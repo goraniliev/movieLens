e89c3a25b7f5374dd42d02d9e348e26e8693a93a
Your Name <goraniliev93@yahoo.com>
2015-09-13 01:23:45 +0200

    printing to file added to randomForest.R

--- R/randomForest.R
+++ R/randomForest.R
@@ -1,28 +1,37 @@
 library(randomForest)
 
+# loading data
 source('preprocessing.R')
+# important features for the classification
 importantFeatures = c('age', 'gender', 'occupation', as.character(data.genres[2:nrow(data.genres)]$nameGenre), 'rating')
-modelData = data.userMovieRating[, importantFeatures, with = F]
-head(modelData)
 
-ind = sample(2, nrow(modelData), replace=TRUE, prob=c(0.7, 0.3))
+# loading data which will be used for model generation
+modelData = data.userMovieRating[, importantFeatures, with = F]
+
+# splitting data into 80% train set and 20% test set
+ind = sample(2, nrow(modelData), replace=TRUE, prob=c(0.8, 0.2))
 trainData = modelData[ind==1, ]
 testData = modelData[ind==2, ]
 
-dim(trainData)
-dim(testData)
-
+# use all important data to predict the rating
 formula = as.factor(rating) ~ age + gender + occupation + Action + Adventure + Animation + Children +   
   + Comedy + Crime + Documentary + Drama + Fantasy + FilmNoir + Horror +     
   + Musical + Mystery + Romance + SciFi + Thriller + War + Western
 
+# build the random forest
 rf = randomForest(formula, data = trainData, ntree = 100)
 
-#resp = treeresponse(moviesCtree, newdata = testData)
-
+# stores true results for the test data
 trueResults = as.factor(testData$rating)
 
+# stores the predicted ratings by the previously created random forest rf
 predicted = predict(rf, newdata = testData) 
-table(predicted, trueResults)
 
+# summarization of the classification
 summarization = confusionMatrix(predicted, trueResults)
+
+# printing results to file
+sink(file = "out/randomForest/summarization.txt", type = "output")
+cat("Random Forest summarization details:\n\n\n")
+summarization
+cat()
