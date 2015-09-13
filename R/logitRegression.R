03177d506690d30c56698702098ba241a03e7101
Your Name <goraniliev93@yahoo.com>
2015-09-13 02:11:28 +0200

    output of logitRegression set to file

--- R/logitRegression.R
+++ R/logitRegression.R
@@ -3,22 +3,39 @@ source('preprocessing.R')
 head(norm.userMovieRating)
 importantFeatures = c('age', 'gender', as.character(data.genres[2:nrow(data.genres)]$nameGenre), 'rating')
 
+# choose important feature to create the model data
 modelData = norm.userMovieRating[, importantFeatures, with = F]
 # converting male and female to 1 and 2 before applyin KNN
 modelData$gender = as.numeric(modelData$gender)
 
-ind = sample(2, nrow(modelData), replace=TRUE, prob=c(0.7, 0.3))
+# ratings should not be normalized
+modelData$rating = data.userMovieRating$rating
+
+# splitting data into 80% train and 20% test set
+ind = sample(2, nrow(modelData), replace=TRUE, prob=c(0.8, 0.2))
 trainData = modelData[ind==1, ]
 testData = modelData[ind==2, ]
 
+# use the different features to predict the rating
 formula = rating ~ age + gender +  Action + Adventure + Animation + Children +
   + Comedy + Crime + Documentary + Drama + Fantasy + FilmNoir + Horror +     
   + Musical + Mystery + Romance + SciFi + Thriller + War + Western
 
 
 library(nnet)
+# create multinom regression model
+# Fits multinomial log-linear models via neural networks.
 ml = multinom(formula, data = trainData)
+
+# make predictions on the test data
 predicted = predict(ml, newdata = testData)
-predicted
+# store true results
 trueResults = testData$rating
+
+# store summary for the created model
 summarization = confusionMatrix(as.factor(predicted), as.factor(trueResults))
+# writing summarization to file
+sink(file = "out/logitRegression/summarization.txt", type = "output")
+cat("Logistic Regression Summarization:\n\n\n")
+summarization
+sink()
