1dac3c5992f7f63aac93d3345f45b49db067abcc
Your Name <goraniliev93@yahoo.com>
2015-09-14 00:04:14 +0200

    AssociationRules.R modified to print in files

--- R/AssocitationRules.R
+++ R/AssocitationRules.R
@@ -1,5 +1,9 @@
 library(arules)
 library(arulesViz)
+
+setwd('/home/goran/git/movieLens/movieLens/R')
+
+# loading data important for the associations
 itemsData = read.table('data/itemsData.txt', header = T, sep = '\t', quote = "")
 
 # t is amatrix representation of the movie genres of each movie (binary matrix)
@@ -7,11 +11,19 @@ t = data.matrix(itemsData[, -1])
 # the initial rules generated (we could expiriment with the params and obtain different rules)
 rules = apriori(t, parameter = list(supp = 0.01, conf = 0.1, target = "rules", minlen = 2))
 # checking the produced rules
+
+# printing all rules
+sink(file = "out//AssociationRules/allRules.txt", type = "output")
+cat('All rules (the redundant are not removed:\n\n\n')
 inspect(rules) # 54 in total, some redundant
-# sorting the rules by lift in decending order
+sink()
+
 rules.sorted = sort(rules, by = "lift")
+sink(file = "out//AssociationRules/allRulesSortedByLift.txt", type = "output")
+# sorting the rules by lift in decending order
 # checking the sorted list
 inspect(rules.sorted) # we can notice many redundant rules
+sink()
 
 # finding redundant rules
 subset.matrix <- is.subset(rules.sorted, rules.sorted)
@@ -21,5 +33,12 @@ redundant <- colSums(subset.matrix, na.rm=T) >= 1
 which(redundant) # checking which are redundant associations
 
 rules.pruned = rules.sorted[!redundant] # removing the redundant rules
-inspect(rules.pruned) # checking the non-redundant 32 rules
+# Printing non-redundant rules into file
+sink(file = "out/AssociationRules/prunedRules.txt")
+inspect(rules.pruned)
+sink()
+
+# store the image for associtation rules
+jpeg('out/AssociationRules/GenresAssociationRules.jpg')
 plot(rules.pruned, method = "graph")
+dev.off()
